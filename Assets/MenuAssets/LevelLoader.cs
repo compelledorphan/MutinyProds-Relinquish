ab068eeb1405e92aa394bc7302c59c44acf47f7d
Liam Simmons <liam@liamsimmons.com>
2016-10-10 17:26:03 -0700

    Effects and Loading Screen
    Gun, effects, loading screen

---
+++ Assets/MenuAssets/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadLevel(string LevelName)
+	{
+		Application.LoadLevel(LevelName);
+	}
+
+	public void RestartLevel()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
+		//UnityEditor.EditorApplication.isPlaying = false;
+	}
+}
